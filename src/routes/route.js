88b24d4c4514387848a88bd5702df42a094f9dd1
Ankit <ankit56mittal@gmail.com>
2022-08-09 00:19:29 +0530

    new assignment  8 aug  addes

--- src/routes/route.js
+++ src/routes/route.js
@@ -35,4 +35,117 @@ router.get('/student-details/:name', function(req, res){
     res.send('Dummy response')
 })
 
+      router.get('/movies', function (req, res){
+    
+    let movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    res.send(movies)
+})
+
+router.get('/movies/:indexNumber', function(req, res){
+   
+
+    
+
+    
+                 // let prgms=    req.params
+             // let out=    console.log(JSON.parse.stringif(prgms));
+                
+              let movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+              let final=req.params.indexNumber
+              let last=movies[final]
+              if(final>movies.length-1){
+  return res.send("given value is greater than array's index");}
+   else {
+
+  return  res.send(last);}
+})
+router.get('/films', function (req, res){
+    
+ let film=   [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       
+       
+    res.send(film);
+})
+    
+      
+router.get('/movies/:indexNumber', function(req, res){
+   
+
+    
+
+    
+    // let prgms=    req.params
+// let out=    console.log(JSON.parse.stringif(prgms));
+   
+ let movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+ let final=req.params.indexNumber
+ let last=movies[final]
+ if(final>movies.length-1){
+return res.send("given value is greater than array's index");}
+else {
+
+return  res.send(last);}
+})
+router.get('/films', function (req, res){
+
+let film=   [ {
+"id": 1,
+"name": "The Shining"
+}, {
+"id": 2,
+"name": "Incendies"
+}, {
+"id": 3,
+"name": "Rang de Basanti"
+}, {
+"id": 4,
+"name": "Finding Nemo"
+}]
+
+
+res.send(film);
+})
+       
+router.get('/films/:filmId', function (req, res){
+
+   
+     let index =req.params.filmId
+     let index1=JSON.stringify(index)
+     let indexlast =JSON.parse(index1)
+    
+    let film=   [ {
+        "id": 1,
+        "name": "The Shining"
+        }, {
+        "id": 2,
+        "name": "Incendies"
+        }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+        }, {
+        "id": 4,
+        "name": "Finding Nemo"
+        }]
+        if(indexlast>film.length){
+            return  res.send('No movie exists with this id')
+        }else{
+       return  res.send(film[indexlast-1]);
+        
+        }
+
+    })
+    
+
 module.exports = router;
