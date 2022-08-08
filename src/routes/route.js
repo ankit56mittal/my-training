b4b21e5f3060925bb39b687497d06461ae10759c
Ankit <ankit56mittal@gmail.com>
2022-08-08 14:42:53 +0530

    chunk tail union  fromPairs added and lodash

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const lodash = require('lodash');
 const abc = require('../introduction/intro')
  const xyz = require('../logger/logger')
  const pqr = require('../util/helper.js')
@@ -12,7 +13,22 @@ router.get('/test-me', function (req, res) {
    pqr.result1 ()
    pqr.info()
    jkl.strings()
-   
+    const months=["jan","feb","mar","april","may","june","july","aug","sept","oct","nov","dec"];
+   console.log(lodash.chunk(months, [size=3]));
+  // console.log(chunks);
+  const odd=[1,3,5,7,9,11,13,15,17,19];
+  console.log(lodash.tail(odd));
+
+  const arr1=[1,2,2,3];
+  const arr2=[4,5,5,6];
+  const arr3=[7,8,8,9];
+  const arr4=[10,12,12,13];
+  const arr5=[14,15,15,16];
+  console.log(lodash.union(arr1,arr2,arr3,arr4,arr5));
+  const arrr =[["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+ 
+  console.log(lodash.fromPairs(arrr));
+
     res.send('My second ever api!')
 });
 
