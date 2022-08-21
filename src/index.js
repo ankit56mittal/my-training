40f2120fbefadb36e8c9dbe113d4d143f360f1da
Ankit <ankit56mittal@gmail.com>
2022-08-22 00:28:06 +0530

    added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Ankit56:0AdBv2SlzhxufMPj@cluster0.wxyttbt.mongodb.net/Ankit_Mittal-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -2,16 +2,25 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId
 
 const bookSchema = new mongoose.Schema( {
-    name: String,
-    author_id: {
+
+
+	name:String,
+    author: {
         type: ObjectId,
-        ref: "Author"
-    }, 
-    price: Number,
-    ratings: Number
-
-
+        ref: "newauthor"
+    },
+	 price : Number,
+		ratings:Number,
+        publisher: {
+            type: ObjectId,
+            ref:"newpublisher"
+        }
+        // ,
+        // IsHardCover:{
+        //     type:Boolean,
+        //     default:false
+        // }
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('LibraryBook', bookSchema)
+module.exports = mongoose.model('newBooks', bookSchema)
