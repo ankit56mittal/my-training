91d6f1a52958e8e8e1af40a6bbebb5d31df9aac1
Ankit <ankit56mittal@gmail.com>
2022-08-30 20:29:52 +0530

    file added

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,20 @@
-const authenticate = function(req, req, next) {
+const jwt = require("jsonwebtoken");
+const authenticate = function(req, res, next) {
     //check the token in request header
-    //validate this token
+    
+    
+     let token = req.headers["x-Auth-token"];
+        if (!token) token = req.headers["x-auth-token"];
+      
+        //If no token is present in the request header return error
+        if (!token) return res.send({ status: false, msg: "token must be present" });
+      
+        console.log(token);
+        //validate this token
+        let decodedToken = jwt.verify(token, "functionup-plutonium");
+        if (!decodedToken)
+          return res.send({ status: false, msg: "token is invalid" });
+           req.loginId =decodedToken.userId
 
     next()
 }
@@ -8,5 +22,17 @@ const authenticate = function(req, req, next) {
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
+  
+
+    let requestedId = req.params.userId;
+    if(requestedId!==req.loginId){
+        console.log(requestedId , req.loginId)
+        return res.send({status:false,msg:" not allowed"})
+    }
+
     next()
+    // console.log(next())
 }
+ 
+module.exports.authenticate=authenticate
+module.exports.authorise=authorise
