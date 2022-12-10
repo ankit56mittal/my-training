b0d323eee2313259ff366bca7ecb00e49c51078b
PritalRupnur <pritalrupnur29012000@gmail.com>
2022-12-10 18:22:47 +0530

    update student

--- src/controllers/studentsControllers.js
+++ src/controllers/studentsControllers.js
@@ -9,15 +9,15 @@ const filledMarks = async (req, res) => {
   try {
     let teacherId = req.params.teacherId;
     let { name, data } = req.body;
-    let {subject, marks} = data;
+    let { subject, marks } = data;
 
     if (data.length === 0) {
       return res.status(400).send({ status: false, message: "fill students subject and marks data" });
     }
     if (!isValidObjectId(teacherId)) {
-        return res.status(400).send({ status: false, message: " Please!! input a valid Id :(" });
-      }
-  
+      return res.status(400).send({ status: false, message: " Please!! input a valid Id :(" });
+    }
+
     if (!name)
       return res.status(400).send({ status: false, message: "Please Enter student Name" });
 
@@ -36,27 +36,102 @@ const filledMarks = async (req, res) => {
     // if (!isValidMarks(marks))
     //   return res.status(400).send({ status: false, message: "Please Enter Valid marks" });
 
-      let Obj = {
-        teacherId:teacherId,
-        name: name,
-        data:data,
-      }
+    let Obj = {
+      teacherId: teacherId,
+      name: name,
+      data: data,
+    }
 
-      let studentMarks = await studentModel.create(Obj)
-      return res.status(201).send({ status:true, message: studentMarks});
+    let studentMarks = await studentModel.create(Obj)
+    return res.status(201).send({ status: true, message: studentMarks });
 
-    } catch (err) {
-       return  res.status(500).send({ status: false, message: err.message });
-      }
-    };
+  } catch (err) {
+    return res.status(500).send({ status: false, message: err.message });
+  }
+};
 
 //=======================================================>>>
 
-const updateStudent = async function(req, res){
-  
+const updateStudent = async function (req, res) {
+  try {
+    //teacherId, name, data(marks, subject)
+
+    let teacherId = req.params.teacherId;
+    let studentId = req.params.studentId
+    let { name, data } = req.body;
+    let { subject, marks } = data[0];
+    let filter = {  }
+
+    // if (data.length === 0) {
+    //   return res.status(400).send({ status: false, message: "fill students subject and marks data" });
+    // }
+    if (!isValidObjectId(teacherId)) {
+      return res.status(400).send({ status: false, message: " Please!! input a valid Id :(" });
+    }
+
+    const studentExist = await studentModel.findOne({ _id: studentId })
+    if (!studentExist) {
+      return res.status(404).send({ status: false, message: " student does not exist" })
+    }
+    console.log(studentId)
+    console.log(studentExist.teacherId)
+    console.log(teacherId)
+    console.log(studentExist)
+
+    if(studentExist.teacherId!=teacherId) {
+      return res.status(404).send({ status: false, message: "studentId and teacher id does not match" })
+    }
+
+
+    if (name) {
+      if (!isValidName(name))
+        return res.status(400).send({ status: false, message: "Please Enter Valid Name" });
+
+      filter.name = name
+    }
+
+    //if theacher is entering the marks of the subject that is already entered
+
+    if (subject && marks) {
+
+
+
+      //for checking if the subject aleady exist in the data
+      const subjectExist = studentExist.data.findIndex(data => data.subject == subject);
+
+      if (subjectExist !== -1) {
+
+        // filter.data = { "data.subject": subject, $inc: { marks: +marks } }
+
+        const increaseMarks = await studentModel.findOneAndUpdate({_id: studentId, "data.subject": subject },
+        { $inc: { "data.$.marks": +marks } }, { new: true });
+
+      }
+      else {
+
+        // filter.data = { $push: { subject: subject, marks: marks } }
+
+        const updateMarks = await studentModel.findOneAndUpdate({ teacherId: teacherId },
+        { $push: {"data":{  subject:subject, marks: marks } }}, { new: true });
+
+      }
+
+      let updatedStudent = await studentModel.findOneAndUpdate({ _id: studentId }, filter, { new: true })
+
+      return res.status(200).send({ status: true, message: "Student details updated successfully", data: updatedStudent })
+
+    }
+  }
+  catch (err) {
+    return res.status(500).send({ status: false, message: err.message });
+  }
+
 }
 
 
 
 
-    module.exports = {filledMarks};
+
+
+
+module.exports = { filledMarks, updateStudent };
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,8 @@ router.post('/createUser', userController.createUser);
 router.post('/userLogin', userController.userLogin);
 router.post('/filledMarks/:teacherId', studentsController.filledMarks);
 
+router.post("/updatestudent/:teacherId/:studentId", studentsController.updateStudent)
+
 //API for wrong route-of-API
 router.all("/*", function (req, res) {
     res.status(400).send({
